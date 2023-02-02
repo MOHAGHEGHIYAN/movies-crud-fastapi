991b5027ad77c3129e42638f9aee4eb99bcc2abd
Hassan Mohagheghiyan <h.mohagheghiyan@gmail.com>
2023-02-02 09:36:04 +0330

    add movies list API

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+movies = [
+    {"id": 1, "title": "The Shawshank Redemption", "genres": ["Crime", "Drama"]},
+    {"id": 2, "title": "The Godfather", "genres": ["Crime", "Drama"]},
+]
+
+
 @app.get("/")
 async def root():
     return {"message": "hello, world!"}
+
+
+@app.get("/movies")
+def get_movies():
+    return movies
